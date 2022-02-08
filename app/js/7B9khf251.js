1a236ee2381e009c22b3adccbc0b2fb0357862ff
kreska <73658321+Kresiaqq@users.noreply.github.com>
2022-02-08 15:51:33 +0100

    Update and rename player.js to 7B9khf251.js

--- app/js/7B9khf251.js
+++ app/js/7B9khf251.js
@@ -6,7 +6,7 @@
 
 const audioPlayer = document.querySelector(".audio-player");
 const audio = new Audio(
-  "audio"
+  "https://sluchajse.ml/lib/music/stickxr-hej.mp3"
 );
 //credit for song: Adrian kreativaweb@gmail.com
 
